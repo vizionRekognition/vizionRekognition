2d7244c550a77115f60060699f966ee1ec1020e6
Mark Mayfield <40775721+themarcusaurelius@users.noreply.github.com>
2019-09-03 23:39:16 -0500

    Update upload.js

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const axios = require('axios')
 const router = express.Router();
 const client = require('../../elasticsearch/connection');
 
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({ 
-    cloud_name: 'ddarvbfef', 
-    api_key: '216782351853344', 
-    api_secret: 'ew3UlLV4_6-Ft3z9zpVsRpLtpT0'
+    cloud_name: 'dbo4wi1ai', 
+    api_key: '447564735638479', 
+    api_secret: 'fnfiQzfPWFdEm8ixDZ3wfZBsm74'
 });
 
 router.post('/', (req, res) => {
@@ -15,7 +16,7 @@ router.post('/', (req, res) => {
   const promises = values.map(image => cloudinary.uploader.upload(image.path, 
     { 
       categorization: "aws_rek_tagging",
-      auto_tagging: 0.6  
+      auto_tagging: 0.7  
     }
   ))
 
@@ -27,35 +28,39 @@ router.post('/', (req, res) => {
   Promise
     .all(promises)
     .then(image => {
-        client.index({  
-            index: 'rekognition',
-            id: image[0].public_id,
-            type: image[0].resource_type,
-            body: {
-            //"version": image[0].version,
-            //"signature": image[0].signature,
-            "width": image[0].width,
-            "height": image[0].height,
-            "resource_type": image[0].resource_type,
+
+      var geoip = [];
+    
+      axios({
+        url: 'https://api.muctool.de/whois',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+      })
+      .then(res => {
+        geoip.push(res.data)
+
+        client.index({ 
+          index: 'rekognition',
+          id: image[0].public_id,
+          type: 'image',
+          body: {
             "created_at": image[0].created_at,
-            "tags": image[0].tags,
-            "bytes": image[0].bytes,
-            "etag": image[0].etag,
+            "etag": image[0].tags,
             "url": image[0].url,
             "secure_url": image[0].secure_url,
             "results": image[0].info.categorization.aws_rek_tagging.data,
-            // "results1": image[0].info.categorization.aws_rek_tagging.data[1],
-            // "results2": image[0].info.categorization.aws_rek_tagging.data[2],
-            // "results3": image[0].info.categorization.aws_rek_tagging.data[3],
-            // "results4": image[0].info.categorization.aws_rek_tagging.data[4],
-            // "results5": image[0].info.categorization.aws_rek_tagging.data[5],
-            // "results6": image[0].info.categorization.aws_rek_tagging.data[6],
-            // "tag": image[0].info.categorization.aws_rek_tagging.data.tag,
-            // "rating": image[0].info.categorization.aws_rek_tagging.data.confidence
-            }
+            "city": geoip[0].city,
+            "country": geoip[0].country,
+            "region": geoip[0].subdivisionIso,
+            "geo_point": [ geoip[0].latitude, geoip[0].longitude ],
+            "ip": geoip[0].ip
+          },
         }, (err, res, status) => {
             console.log('Photo Uploaded TO Vizion.ai!');
         })
+      })
     })
     .catch(err => {
       console.log(err);
